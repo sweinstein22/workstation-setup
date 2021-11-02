3973e602a06b9b690679b67ef876bd2ee3aa1917
Sarah Weinstein <sweinstein@pivotal.io>
2021-11-02 16:04:41 -0700

    Automate ccng database setup steps

--- cloud-foundry/ccng.sh
+++ cloud-foundry/ccng.sh
@@ -3,11 +3,18 @@
 brew install chruby
 brew install ruby@2.7
 brew install mysql
-sudo gem install bundler:2.1.4
+brew install postgresql
 
 if [ ! -e ~/workspace/capi-release/src/cloud_controller_ng/README.md ]; then
   pushd ~/workspace
     rm -rf capi-release
     git clone git@github.com:cloudfoundry/capi-release.git --recurse-submodules
+
+    sudo gem install bundler:2.1.4
+    # gem install mysql2 -- --srcdir=/usr/local/mysql/include
+
+    brew services start postgresql
+    createuser -s postgres
+    DB=postgres rake db:create
   popd
 fi
